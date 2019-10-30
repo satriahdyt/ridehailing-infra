2d4506981ec14578d2f85c7b58918df0cf80cb42
Muhammad Hasan <hasan@sirus.dev>
2019-10-30 09:17:32 +0700

    add ddos script

---
+++ ddos/index.js
@@ -0,0 +1,51 @@
+const https = require("http");
+
+function makeRequest() {
+  return new Promise((resolve, reject) => {
+    const req = https.request("http://localhost:30001", res => {
+      res.once("data", datas => {
+        const data = JSON.parse(datas);
+        resolve(data[1].ip);
+      });
+    });
+
+    req.once("error", error => {
+      reject(error);
+    });
+
+    req.end();
+  });
+}
+
+function delay(time) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), time || 3000);
+  });
+}
+
+const MAX_REQUEST = 200;
+const DELAY = 100;
+
+async function normal() {
+  while(1) {
+    await delay(DELAY);
+    const ip = await makeRequest();
+    console.log(ip);
+  }
+}
+
+async function ddos() {
+  const reqs = [];
+  for (let i = 0; i < MAX_REQUEST; i++) {
+    reqs.push(i);
+  }
+  while(1) {
+    await delay(DELAY);
+    const ips = await Promise.all(reqs.map(() => makeRequest()));
+    for (let ip in ips) {
+      console.log(ips[ip]);
+    }
+  }
+}
+
+ddos();
